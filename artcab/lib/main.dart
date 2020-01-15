8dade9880b2644fda075796df9ea81e62a7d63f8
Luke-Vader <32070588+Luke-Vader@users.noreply.github.com>
2020-01-15 15:03:06 +0530

    kya mast
    Co-Authored-By: Sanket Mandal <sanketmndl@users.noreply.github.com>

--- artcab/lib/main.dart
+++ artcab/lib/main.dart
@@ -1,52 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() {
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
-
-  Widget titleSection = Container(
-    padding: const EdgeInsets.all(31),
-    child: Row(
-      children: <Widget>[
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: Text(
-                  'Something Up',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold
-                  ),
-                ),
-              ),
-              Text(
-                'Something Down',
-                style: TextStyle(
-                  color: Colors.green[100],
-                ),
-              )
-            ],
-          ),
-        )
-        
-      ],
-    ),
-  );
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Layout Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Layout Demo'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
+    return new MaterialApp(
+      title: 'Login',
+      theme: new ThemeData(
+        primarySwatch: Colors.red;
       ),
+      home: new LoginPage(),
     );
   }
 }
