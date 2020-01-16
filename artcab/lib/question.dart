1e0adcd4edd95c118ae7e17b302795a2539de0e4
Luke-Vader <32070588+Luke-Vader@users.noreply.github.com>
2020-01-16 11:19:20 +0530

    Update question.dart
    did some more shit to the quiestions in the single screen, The variable are yet to be made
    
    Co-Authored-By: Sanket Mandal <sanketmndl@users.noreply.github.com>

--- artcab/lib/question.dart
+++ artcab/lib/question.dart
@@ -6,57 +6,180 @@ class Questionnaire extends StatefulWidget {
   _QuestionnaireState createState() => _QuestionnaireState();
 }
 
+enum FormType { specialisation, cinema, genre }
+
 class _QuestionnaireState extends State<Questionnaire> {
+  final questionColor = Colors.white;
+  final formKey = new GlobalKey<FormState>();
+  FormType _formType = FormType.specialisation;
 
-  void skip() {
-    
-
+  void moveToGenre() {
+    formKey.currentState.reset();
+    setState(() {
+      _formType = FormType.genre;
+    });
   }
 
-  final questionColor = Colors.white;
+  void movetoTaste() {
+    formKey.currentState.reset();
+    setState(() {
+      _formType = FormType.cinema;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            new Flexible(
-              child: Container(),
-              flex: 5,
+        child: new Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: buildSpecialisation(),
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> buildSpecialisation() {
+    if (_formType == FormType.specialisation) {
+      return [
+        new Flexible(
+          child: Container(),
+          flex: 5,
+        ),
+        new Flexible(
+          child: new Container(
+            padding: const EdgeInsets.all(16.0),
+            child: new Text(
+              'Your Specialisation?',
+              style: TextStyle(
+                fontSize: 36,
+                color: questionColor,
+              ),
             ),
-            new Flexible(
-              child: new Container(
-                padding: const EdgeInsets.all(16.0),
+          ),
+          flex: 3,
+        ),
+        new Flexible(
+          child: new Container(
+            padding: const EdgeInsets.all(16.0),
+            child: new TextFormField(
+              decoration: new InputDecoration(
+                  fillColor: questionColor,
+                  filled: true,
+                  border: OutlineInputBorder()),
+            ),
+          ),
+          flex: 3,
+        ),
+        new Flexible(
+          child: Container(
+            child: new Center(
+              child: new FlatButton(
                 child: new Text(
-                  'Your Specialisation?',
+                  'SKIP',
                   style: TextStyle(
-                    fontSize: 36,
+                    fontSize: 14,
                     color: questionColor,
                   ),
                 ),
+                onPressed: moveToGenre,
               ),
-              flex: 3,
             ),
-            new Flexible(
-              child: new Container(
-                padding: const EdgeInsets.all(16.0),
-                child: new TextFormField(
-                  decoration: new InputDecoration(
-                    fillColor: questionColor,
-                    filled: true,
-                    border: OutlineInputBorder()
+          ),
+          flex: 5,
+        )
+      ];
+    } else if (_formType == FormType.genre) {
+      return [
+        new Flexible(
+          child: Container(),
+          flex: 5,
+        ),
+        new Flexible(
+          child: new Container(
+            padding: const EdgeInsets.all(16.0),
+            child: new Text(
+              'Your Preferred Genre?',
+              style: TextStyle(
+                fontSize: 36,
+                color: questionColor,
+              ),
+            ),
+          ),
+          flex: 3,
+        ),
+        new Flexible(
+          child: new Container(
+            padding: const EdgeInsets.all(16.0),
+            child: new TextFormField(
+              decoration: new InputDecoration(
+                  fillColor: questionColor,
+                  filled: true,
+                  border: OutlineInputBorder()),
+            ),
+          ),
+          flex: 3,
+        ),
+        new Flexible(
+          child: Container(
+            child: new Center(
+              child: new FlatButton(
+                child: new Text(
+                  'SKIP',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: questionColor,
                   ),
                 ),
+                onPressed: movetoTaste,
               ),
-              flex: 3,
             ),
-            new Flexible(
-              child: Container(
-                child: new Center(
-                  child: new FlatButton(
+          ),
+          flex: 5,
+        )
+      ];
+    } else {
+      return [
+        new Flexible(
+          child: Container(),
+          flex: 5,
+        ),
+        new Flexible(
+          child: new Container(
+            padding: const EdgeInsets.all(16.0),
+            child: new Text(
+              'Your Cinema Taste?',
+              style: TextStyle(
+                fontSize: 36,
+                color: questionColor,
+              ),
+            ),
+          ),
+          flex: 3,
+        ),
+        new Flexible(
+          child: new Container(
+            padding: const EdgeInsets.all(16.0),
+            child: new TextFormField(
+              decoration: new InputDecoration(
+                  fillColor: questionColor,
+                  filled: true,
+                  border: OutlineInputBorder()),
+            ),
+          ),
+          flex: 3,
+        ),
+        new Flexible(
+          child: Container(
+            child: new Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  new FlatButton(
                     child: new Text(
                       'SKIP',
                       style: TextStyle(
@@ -64,20 +187,35 @@ class _QuestionnaireState extends State<Questionnaire> {
                         color: questionColor,
                       ),
                     ),
-                    onPressed: () => {
+                    onPressed: () {
                       Navigator.push(
                         context,
                         MaterialPageRoute(builder: (context) => LoginPage()),
-                      ),
+                      );
                     },
                   ),
-                ),
+                  new FlatButton(
+                    child: new Text(
+                      'NEXT',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: questionColor,
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginPage()),
+                      );
+                    },
+                  )
+                ],
               ),
-              flex: 5,
-            )
-          ],
-        ),
-      ),
-    );
+            ),
+          ),
+          flex: 5,
+        )
+      ];
+    }
   }
 }
