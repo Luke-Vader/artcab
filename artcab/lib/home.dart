f6a17d0bd18c39f121db04ec8dab8bc8a50c0189
Luke-Vader <32070588+Luke-Vader@users.noreply.github.com>
2020-01-24 13:43:59 +0530

    added the bottom navigation to the homepage
    lol
    
    Co-Authored-By: Sanket Mandal <sanketmndl@users.noreply.github.com>

--- artcab/lib/home.dart
+++ artcab/lib/home.dart
@@ -6,11 +6,68 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  int _cIndex = 0;
+
+  void _incrementTab(index) {
+    setState(() {
+      _cIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('HomePage'),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _cIndex,
+        backgroundColor: Colors.black,
+        type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Colors.black,
+            icon: new Icon(
+              Icons.filter_list,
+              color: Colors.white,
+            ),
+            title: new Text(
+              'Feed',
+              style: TextStyle(
+                color: Colors.white
+              ),
+            )
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.black,
+            icon: new Icon(
+              Icons.lightbulb_outline,
+              color: Colors.white,
+            ),
+            title: new Text(
+              'Ideas',
+              style: TextStyle(
+                color: Colors.white
+              ),
+            )
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.black,
+            activeIcon: new Icon(Icons.person, color: Colors.white,),
+            icon: new Icon(
+              Icons.person_outline,
+              color: Colors.white,
+            ),
+            title: new Text(
+              'Profile',
+              style: TextStyle(
+                color: Colors.white
+              ),
+            )
+          ),
+        ],
+        onTap: (index) {
+          _incrementTab(index);
+        },
       ),
     );
   }
