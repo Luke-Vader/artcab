71786c1483e6941d99e6d999fe3abb9346577bd1
Luke-Vader <32070588+Luke-Vader@users.noreply.github.com>
2020-01-25 00:41:25 +0530

    Update your_specialisation.dart

--- artcab/lib/registration_pages/your_specialisation.dart
+++ artcab/lib/registration_pages/your_specialisation.dart
@@ -7,13 +7,12 @@ class Special extends StatefulWidget {
 }
 
 class _SpecialState extends State<Special> {
-
   final questionColor = Colors.white;
   final formKey = new GlobalKey<FormState>();
-  
+
   void validateSend() {
     final form = formKey.currentState;
-    if(form.validate()) {
+    if (form.validate()) {
       form.save();
       print('Valid');
       /*Navigator.push(
@@ -41,34 +40,34 @@ class _SpecialState extends State<Special> {
 
   _showDialog() {
     showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Specialisation'),
-          content: MultiSelectChip(
-            specialList,
-            onSelectionChanged: (selectedList) {
-              setState(() {
-                selectedSpecials = selectedList;
-              });
-            },
-          ),
-          actions: <Widget>[
-            FlatButton(
-              child: Text("Save"),
-              onPressed: () {
-                Navigator.of(context).pop();
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (BuildContext context) => SCategory(categories: selectedSpecials))
-                );
-                print(selectedSpecials.length);
-              }, 
-            )
-          ],
-        );
-      }
-    );
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Specialisation'),
+            content: MultiSelectChip(
+              specialList,
+              onSelectionChanged: (selectedList) {
+                setState(() {
+                  selectedSpecials = selectedList;
+                });
+              },
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Save"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) =>
+                              SCategory(categories: selectedSpecials)));
+                  print(selectedSpecials.length);
+                },
+              )
+            ],
+          );
+        });
   }
 
   @override
@@ -78,83 +77,76 @@ class _SpecialState extends State<Special> {
         body: new Form(
           key: formKey,
           child: new Column(
-          children: <Widget>[
-            new Flexible(
-              child: Container(),
-              flex: 5,
-            ),
-            new Flexible(
-              child: new Container(
-                padding: const EdgeInsets.all(16.0),
-                child: new Text(
-                  'Your Specialisation?',
-                  style: TextStyle(
-                    fontSize: 32,
-                    color: questionColor,
-                  ),
-                ),
+            children: <Widget>[
+              new Flexible(
+                child: Container(),
+                flex: 5,
               ),
-              flex: 3,
-            ),
-            new Flexible(
-              child: new Container(
-                padding: const EdgeInsets.all(16.0),
-                child: FlatButton(
-                  child: Text(
-                    'Tap to select one or more',
+              new Flexible(
+                child: new Container(
+                  padding: const EdgeInsets.all(16.0),
+                  child: new Text(
+                    'Your Specialisation?',
                     style: TextStyle(
-                      fontSize: 14,
-                      color: Colors.grey,
+                      fontSize: 32,
+                      color: questionColor,
                     ),
                   ),
-                  onPressed: () => _showDialog(),
                 ),
+                flex: 3,
               ),
-              flex: 3,
-            ),
-            new Flexible(
-              child: new Center(
-                  child: new FlatButton(
-                child: new Text(
-                  'NEXT',
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: questionColor,
+              new Flexible(
+                child: new Container(
+                  padding: const EdgeInsets.all(16.0),
+                  child: FlatButton(
+                    child: Text(
+                      'Tap to select one or more',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    onPressed: () => _showDialog(),
                   ),
                 ),
-                onPressed: validateSend,
-              )),
-              flex: 5,
-            )
-          ],
-        ),
-      )
-    );
+                flex: 3,
+              ),
+              new Flexible(
+                child: new Center(
+                    child: new FlatButton(
+                  child: new Text(
+                    'NEXT',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: questionColor,
+                    ),
+                  ),
+                  onPressed: validateSend,
+                )),
+                flex: 5,
+              )
+            ],
+          ),
+        ));
   }
 }
 
-
 class MultiSelectChip extends StatefulWidget {
-
   final List<String> specialList;
   final Function(List<String>) onSelectionChanged;
 
-  MultiSelectChip(
-    this.specialList,
-    {this.onSelectionChanged}
-  );
+  MultiSelectChip(this.specialList, {this.onSelectionChanged});
 
   @override
   _MultiSelectChipState createState() => _MultiSelectChipState();
 }
 
 class _MultiSelectChipState extends State<MultiSelectChip> {
-
   List<String> selectedSpecials = List();
 
   _buildChoiceList() {
     List<Widget> choices = List();
-    
+
     widget.specialList.forEach((item) {
       choices.add(Container(
         padding: const EdgeInsets.all(2.0),
@@ -163,13 +155,15 @@ class _MultiSelectChipState extends State<MultiSelectChip> {
           selected: selectedSpecials.contains(item),
           onSelected: (selected) {
             setState(() {
-              selectedSpecials.contains(item) ? selectedSpecials.remove(item) : selectedSpecials.add(item);
+              selectedSpecials.contains(item)
+                  ? selectedSpecials.remove(item)
+                  : selectedSpecials.add(item);
               widget.onSelectionChanged(selectedSpecials);
             });
           },
         ),
       ));
-    }); 
+    });
     return choices;
   }
 
