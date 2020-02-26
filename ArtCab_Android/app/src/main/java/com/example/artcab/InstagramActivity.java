456565f7c52dffb4a31a23384c92132dee148856
Luke-Vader <32070588+Luke-Vader@users.noreply.github.com>
2020-02-27 01:23:07 +0530

    instagram is set
    Phone number se start karna hai

--- ArtCab_Android/app/src/main/java/com/example/artcab/InstagramActivity.java
+++ ArtCab_Android/app/src/main/java/com/example/artcab/InstagramActivity.java
@@ -2,6 +2,7 @@ package com.example.artcab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +19,7 @@ public class InstagramActivity extends AppCompatActivity {
     String email;
     EditText instagram;
     Button goToPhone;
+    Button skipInsta;
 
     @Override
     protected void onStart() {
@@ -36,15 +38,41 @@ public class InstagramActivity extends AppCompatActivity {
         setContentView(R.layout.activity_instagram);
         instagram = findViewById(R.id.instagram);
         goToPhone = findViewById(R.id.goto_phone);
+        skipInsta = findViewById(R.id.skip_insta);
 
         goToPhone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                if (instagram.getText().toString().length() == 0) {
+                    instagram.setError("Required");
+                } else {
+                    Bundle bundle = new Bundle();
+                    Intent intent = new Intent(getApplicationContext(), InstagramActivity.class);
+                    bundle.putStringArrayList("specials", specials);
+                    bundle.putStringArrayList("genres", genres);
+                    bundle.putStringArrayList("tastes", tastes);
+                    bundle.putString("name", name);
+                    bundle.putString("email", email);
+                    bundle.putString("instagram", instagram.getText().toString());
+                    intent.putExtras(bundle);
+                    startActivity(intent);
+                }
             }
         });
 
-
-
+        skipInsta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                Intent intent = new Intent(getApplicationContext(), InstagramActivity.class);
+                bundle.putStringArrayList("specials", specials);
+                bundle.putStringArrayList("genres", genres);
+                bundle.putStringArrayList("tastes", tastes);
+                bundle.putString("name", name);
+                bundle.putString("email", email);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
     }
 }
