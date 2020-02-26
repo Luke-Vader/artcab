6d8318936626b68f7dc0a1c08f7c3be19ad014bd
Luke-Vader <32070588+Luke-Vader@users.noreply.github.com>
2020-02-26 15:28:20 +0530

    doing something with reg

--- ArtCab_Android/app/src/main/java/com/example/artcab/ProfileFragment.java
+++ ArtCab_Android/app/src/main/java/com/example/artcab/ProfileFragment.java
@@ -1,9 +1,12 @@
 package com.example.artcab;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,9 +14,26 @@ import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
 
+    Button login;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        login = getActivity().findViewById(R.id.login_button);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Login", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), LoginOrSignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ ArtCab_Android/app/src/main/java/com/example/artcab/SpecialisationActivity.java
@@ -0,0 +1,14 @@
+package com.example.artcab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpecialisationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_specialisation);
+    }
+}
