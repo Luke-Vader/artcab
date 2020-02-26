8d81679a378261ae7c4ba20dcfaacbcf08990b1e
Luke-Vader <32070588+Luke-Vader@users.noreply.github.com>
2020-02-26 07:40:00 +0530

    added the visuals

--- ArtCab_Android/app/src/main/java/com/example/artcab/IdeaFragment.java
+++ ArtCab_Android/app/src/main/java/com/example/artcab/IdeaFragment.java
@@ -11,6 +11,8 @@ import androidx.fragment.app.Fragment;
 
 public class IdeaFragment extends Fragment {
 
+    
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
