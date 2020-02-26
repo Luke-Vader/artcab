c47b79cd3457984806277fbf35ae87113d6fb13f
Luke-Vader <32070588+Luke-Vader@users.noreply.github.com>
2020-02-26 23:49:11 +0530

    specialisation is done
    onto sub categories nw

---
+++ ArtCab_Android/app/src/main/java/com/example/artcab/SpecialSubActivity.java
@@ -0,0 +1,46 @@
+package com.example.artcab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class SpecialSubActivity extends AppCompatActivity {
+
+    Button goToGenre;
+    TextView selectSubSpecials;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Bundle bundle = getIntent().getExtras();
+        ArrayList<String> specials = bundle.getStringArrayList("specials");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_special_sub);
+        goToGenre = findViewById(R.id.goto_genre);
+        selectSubSpecials = findViewById(R.id.sub_special_select);
+
+        selectSubSpecials.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        goToGenre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+}
--- ArtCab_Android/app/src/main/java/com/example/artcab/SpecialisationActivity.java
+++ ArtCab_Android/app/src/main/java/com/example/artcab/SpecialisationActivity.java
@@ -4,35 +4,52 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class SpecialisationActivity extends AppCompatActivity {
 
     TextView selectSpecials;
     ArrayList<String> specials = new ArrayList<>();
+    String selected;
+    Button goToSpecialSubCat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_specialisation);
         selectSpecials = findViewById(R.id.special_select);
+        goToSpecialSubCat = findViewById(R.id.goto_spc_sub);
 
         selectSpecials.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showSpecials();
+                Toast.makeText(SpecialisationActivity.this, "Specials Selected", Toast.LENGTH_SHORT).show();
             }
         });
 
-        Toast.makeText(this, specials.size(), Toast.LENGTH_SHORT).show();
+        goToSpecialSubCat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putStringArrayList("specials", specials);
+                Intent intent = new Intent(getApplicationContext(), SpecialSubActivity.class);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
     }
 
     private void showSpecials() {
+        selected = "";
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Specialisations");
 
@@ -53,10 +70,8 @@ public class SpecialisationActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialog, int which, boolean isChecked) {
                 if (isChecked) {
-                    // If the user checked the item, add it to the selected items
                     specials.add(specialisations[which]);
                 } else if (specials.contains(specialisations[which])) {
-                    // Else, if the item is already in the array, remove it
                     specials.remove(specialisations[which]);
                 }
             }
@@ -66,7 +81,10 @@ public class SpecialisationActivity extends AppCompatActivity {
         builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
+                for (String special : specials) {
+                    selected += special + " ";
+                }
+                selectSpecials.setText(selected);
             }
         });
         builder.setNegativeButton("Cancel", null);
