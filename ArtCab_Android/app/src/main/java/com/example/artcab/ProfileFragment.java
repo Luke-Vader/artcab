cd2251994ae4c839ccbf59272f8a4515249722a2
Luke-Vader <32070588+Luke-Vader@users.noreply.github.com>
2020-03-01 15:10:40 +0530

    doing things to the profile fragment

--- ArtCab_Android/app/src/main/java/com/example/artcab/ProfileFragment.java
+++ ArtCab_Android/app/src/main/java/com/example/artcab/ProfileFragment.java
@@ -1,26 +1,56 @@
 package com.example.artcab;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 public class ProfileFragment extends Fragment {
 
     Button login;
+    private ImageView instagram;
+    private ImageView whatsApp;
+    private ImageView email;
+    private ImageView profilePicture;
 
-    FirebaseFirestore db;
-    FirebaseAuth auth;
+    User user;
+    TextView name;
+    TextView quote;
+    TextView portfolio;
+    ArrayList<String> link;
+    ArrayList<String> specials;
+    RecyclerView specialisations;
+    RecyclerView links;
+    RelativeLayout signedIn;
+    RelativeLayout signIn;
+
+    private FirebaseFirestore db;
+    private FirebaseAuth auth;
+    private FirebaseStorage storage;
 
     @Nullable
     @Override
@@ -32,11 +62,36 @@ public class ProfileFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        signIn = getActivity().findViewById(R.id.login_interface);
+        signedIn = getActivity().findViewById(R.id.profile_interface);
+        profilePicture = getActivity().findViewById(R.id.user_profile);
+        whatsApp = getActivity().findViewById(R.id.whatsapp_connect);
+        instagram = getActivity().findViewById(R.id.instagram_connect);
+        email = getActivity().findViewById(R.id.email_connect);
+        name = getActivity().findViewById(R.id.profile_name);
+        quote = getActivity().findViewById(R.id.profile_quote);
+        portfolio = getActivity().findViewById(R.id.portfolio_text);
+        specialisations = getActivity().findViewById(R.id.special_recycler);
+        links = getActivity().findViewById(R.id.links_recycler);
+        login = getActivity().findViewById(R.id.login_button);
+
         db = FirebaseFirestore.getInstance();
         auth = FirebaseAuth.getInstance();
+        storage = FirebaseStorage.getInstance();
 
-        /*login = getActivity().findViewById(R.id.login_button);
+        if (auth.getCurrentUser() == null) {
+            signedIn.setVisibility(View.GONE);
+            signIn.setVisibility(View.VISIBLE);
+            login();
+        } else {
+            signedIn.setVisibility(View.VISIBLE);
+            signIn.setVisibility(View.GONE);
+            getData();
+        }
 
+    }
+
+    private void login() {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,6 +99,41 @@ public class ProfileFragment extends Fragment {
                 Intent intent = new Intent(getActivity(), LoginOrSignupActivity.class);
                 startActivity(intent);
             }
-        });*/
+        });
     }
+
+    private void getData() {
+        storage.getReference().child("user/" + auth.getCurrentUser().getUid()).getDownloadUrl()
+                .addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Picasso.get().load(uri).fit().centerCrop().into(profilePicture);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getActivity(), "Profile Photo Missing", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        db.collection("users").document(auth.getCurrentUser().getUid()).get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        user = documentSnapshot.toObject(User.class);
+                        name.setText(user.getName());
+                        quote.setText(user.getQuote());
+                        portfolio.setText(user.getPortfolio());
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getActivity(), "No User Found", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
 }
