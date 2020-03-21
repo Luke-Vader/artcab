e6dd1b44eeadfc6a00d1db77b90107dc688f88c8
Luke-Vader <32070588+Luke-Vader@users.noreply.github.com>
2020-03-21 21:52:07 +0530

    working with the popup user thing

--- ArtCab_Android/app/src/main/java/com/example/artcab/components/NetworkAdapter.java
+++ ArtCab_Android/app/src/main/java/com/example/artcab/components/NetworkAdapter.java
@@ -1,11 +1,14 @@
 package com.example.artcab.components;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -23,6 +26,7 @@ public class NetworkAdapter extends RecyclerView.Adapter<NetworkAdapter.ViewHold
 
     StorageReference storageReference;
 
+    Dialog userDialog;
     ArrayList<User> users;
     Context context;
 
@@ -52,6 +56,14 @@ public class NetworkAdapter extends RecyclerView.Adapter<NetworkAdapter.ViewHold
                     }
                 });
 
+        holder.userCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                displayUser(users.get(holder.getAdapterPosition()));
+            }
+        });
+
     }
 
     @Override
@@ -61,6 +73,7 @@ public class NetworkAdapter extends RecyclerView.Adapter<NetworkAdapter.ViewHold
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+        RelativeLayout userCard;
         TextView name;
         TextView quote;
         ImageView image;
@@ -70,6 +83,43 @@ public class NetworkAdapter extends RecyclerView.Adapter<NetworkAdapter.ViewHold
             name = itemView.findViewById(R.id.network_username);
             quote = itemView.findViewById(R.id.network_quote);
             image = itemView.findViewById(R.id.network_profile_image);
+            userCard = itemView.findViewById(R.id.user_container);
+
         }
     }
+
+    private void displayUser(User user) {
+        userDialog = new Dialog(context, android.R.style.Theme_DeviceDefault_DialogWhenLarge_NoActionBar);
+        userDialog.setContentView(R.layout.user_view);
+        userDialog.show();
+
+        ImageButton close = userDialog.findViewById(R.id.collapse);
+        TextView username = userDialog.findViewById(R.id.profile_name);
+        TextView quote = userDialog.findViewById(R.id.profile_quote);
+        TextView portfolio = userDialog.findViewById(R.id.portfolio_text);
+        final ImageView userImage = userDialog.findViewById(R.id.user_profile);
+        RecyclerView specials = userDialog.findViewById(R.id.special_recycler);
+        RecyclerView links = userDialog.findViewById(R.id.links_recycler);
+
+        storageReference.child("user/" + user.getUserId()).getDownloadUrl()
+                .addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Picasso.get().load(uri).fit().centerCrop().into(userImage);
+                    }
+                });
+
+        username.setText(user.getName());
+        quote.setText(user.getQuote());
+        portfolio.setText(user.getPortfolio());
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userDialog.dismiss();
+            }
+        });
+
+    }
+
 }
