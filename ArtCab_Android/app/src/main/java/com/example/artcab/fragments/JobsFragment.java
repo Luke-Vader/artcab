716d5607f889e0d1be3d3b856155d857d604296c
Luke-Vader <32070588+Luke-Vader@users.noreply.github.com>
2020-03-22 23:50:46 +0530

    Update JobsFragment.java

--- ArtCab_Android/app/src/main/java/com/example/artcab/fragments/JobsFragment.java
+++ ArtCab_Android/app/src/main/java/com/example/artcab/fragments/JobsFragment.java
@@ -4,14 +4,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.artcab.R;
+import com.example.artcab.components.Job;
+import com.example.artcab.components.JobAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class JobsFragment extends Fragment {
+
+    FloatingActionButton addJob;
+    FirebaseAuth auth;
+    FirebaseFirestore db;
+
+    RecyclerView jobsRecyclerView;
+    ArrayList<Job> jobs;
+    JobAdapter jobAdapter;
 
     @Nullable
     @Override
@@ -23,5 +45,37 @@ public class JobsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+        jobsRecyclerView = view.findViewById(R.id.job_container);
+        addJob = view.findViewById(R.id.add_job);
+
+        jobs = new ArrayList<>();
+        db.collection("jobs").get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
+                                Job serverJob = documentSnapshot.toObject(Job.class);
+                                jobs.add(serverJob);
+                            }
+                            setAdapter();
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getActivity(), "No Jobs", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
     }
+
+    private void setAdapter() {
+        jobAdapter =
+    }
+
 }
