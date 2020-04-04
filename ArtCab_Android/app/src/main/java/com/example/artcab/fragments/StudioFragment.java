526e2256da5d3f04ab07eb984f2fba0d6ba8cea5
Luke-Vader <32070588+Luke-Vader@users.noreply.github.com>
2020-04-05 01:05:09 +0530

    location stunt accomplished

--- ArtCab_Android/app/src/main/java/com/example/artcab/fragments/StudioFragment.java
+++ ArtCab_Android/app/src/main/java/com/example/artcab/fragments/StudioFragment.java
@@ -1,14 +1,16 @@
 package com.example.artcab.fragments;
 
 import android.app.Dialog;
-import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -24,6 +26,7 @@ import com.example.artcab.components.StudioAdapter;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.model.Place;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -31,12 +34,16 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.rtchagas.pingplacepicker.PingPlacePicker;
 
 import java.util.ArrayList;
 import java.util.UUID;
 
+import static android.app.Activity.RESULT_OK;
+
 public class StudioFragment extends Fragment {
 
+    int REQUEST_PLACE_PICKER = 71;
     FirebaseFirestore db;
     FirebaseAuth auth;
     StorageReference storage;
@@ -47,7 +54,18 @@ public class StudioFragment extends Fragment {
     ArrayList<Studio> studios;
     StudioAdapter studioAdapter;
     FloatingActionButton addStudio;
-    ImageButton test;
+    //ImageButton test;
+    EditText name;
+    EditText carpetArea;
+    EditText rent;
+    Switch parking;
+    Switch equipment;
+    EditText deposit;
+    EditText description;
+    TextView location;
+    Button addImages;
+    Button post;
+    ImageButton close;
 
     @Nullable
     @Override
@@ -62,12 +80,15 @@ public class StudioFragment extends Fragment {
         db = FirebaseFirestore.getInstance();
         auth = FirebaseAuth.getInstance();
         storage = FirebaseStorage.getInstance().getReference();
+        /*if (!Places.isInitialized()) {
+            Places.initialize(getActivity(), "AIzaSyA1Ij65-z4eFi6eITqDcU5TYE7Q-u6S510");
+        }*/
 
         addStudio = view.findViewById(R.id.add_studio);
         studioRecycler = view.findViewById(R.id.studio_container);
-        test = view.findViewById(R.id.filter_studios);
+        //test = view.findViewById(R.id.filter_studios);
 
-        test.setOnClickListener(new View.OnClickListener() {
+        /*test.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Creates an Intent that will load a map of San Francisco
@@ -76,7 +97,7 @@ public class StudioFragment extends Fragment {
                 mapIntent.setPackage("com.google.android.apps.maps");
                 startActivity(mapIntent);
             }
-        });
+        });*/
 
         studios = new ArrayList<>();
         db.collection("studios").get()
@@ -90,12 +111,6 @@ public class StudioFragment extends Fragment {
                             }
                             setAdapter();
                         }
-                    }
-                })
-                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                    @Override
-                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                        Toast.makeText(getActivity(), "No Studios", Toast.LENGTH_SHORT).show();
                     }
                 });
         studioRecycler.setLayoutManager(new LinearLayoutManager(getActivity()));
@@ -114,14 +129,28 @@ public class StudioFragment extends Fragment {
     }
 
     private void postStudio() {
-        uuid = uuid.randomUUID();
+        uuid = UUID.randomUUID();
 
         View studioView = getLayoutInflater().inflate(R.layout.studio_dialog, null);
         studioDialog = new Dialog(getActivity(), android.R.style.Theme_DeviceDefault_DialogWhenLarge_NoActionBar);
         studioDialog.setContentView(studioView);
         studioDialog.show();
 
+        name = studioView.findViewById(R.id.studio_name);
+        carpetArea = studioView.findViewById(R.id.studio_area);
+        rent = studioView.findViewById(R.id.studio_rent);
+        parking = studioView.findViewById(R.id.studio_parking);
+        equipment = studioView.findViewById(R.id.studio_equipment);
+        deposit = studioView.findViewById(R.id.studio_deposit);
+        description = studioView.findViewById(R.id.studio_description);
+        location = studioView.findViewById(R.id.studio_location);
 
+        location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPlacePicker();
+            }
+        });
 
     }
 
@@ -129,6 +158,39 @@ public class StudioFragment extends Fragment {
         studioAdapter = new StudioAdapter(studios, getActivity());
         studioRecycler.setAdapter(studioAdapter);
         studioAdapter.notifyDataSetChanged();
+    }
+
+    private void showPlacePicker() {
+        PingPlacePicker.IntentBuilder builder = new PingPlacePicker.IntentBuilder();
+        builder.setAndroidApiKey("AIzaSyAv4dlLRkzDZScczNsC0E2XLu_-IBo8VA4")
+                .setMapsApiKey("AIzaSyAv4dlLRkzDZScczNsC0E2XLu_-IBo8VA4");
+
+        // If you want to set a initial location rather then the current device location.
+        // NOTE: enable_nearby_search MUST be true.
+        // builder.setLatLng(new LatLng(37.4219999, -122.0862462))
+
+        try {
+            Intent placeIntent = builder.build(getActivity());
+            startActivityForResult(placeIntent, REQUEST_PLACE_PICKER);
+        }
+        catch (Exception ex) {
+            Toast.makeText(getActivity(), "WELL SHIT", Toast.LENGTH_SHORT).show();
+            ex.printStackTrace();
+            // Google Play services is not available...
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if ((requestCode == REQUEST_PLACE_PICKER) && (resultCode == RESULT_OK)) {
+            Place place = PingPlacePicker.getPlace(data);
+            if (place != null) {
+                String latitude = String.valueOf(place.getLatLng().latitude);
+                String longitude = String.valueOf(place.getLatLng().longitude);
+                location.setText(latitude);
+                Toast.makeText(getActivity(), latitude, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
 }
