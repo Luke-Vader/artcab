e150844991ab6febeee39e1dc38181ca953c6a6b
Luke-Vader <32070588+Luke-Vader@users.noreply.github.com>
2020-04-09 17:13:52 +0530

    edit Profile layout ready

--- ArtCab_Android/app/src/main/java/com/example/artcab/EditUserActivity.java
+++ ArtCab_Android/app/src/main/java/com/example/artcab/EditUserActivity.java
@@ -3,12 +3,25 @@ package com.example.artcab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class EditUserActivity extends AppCompatActivity {
+
+    EditText name;
+    EditText email;
+    EditText instagram;
+    EditText phone;
+    EditText whatsApp;
+    EditText portfolio;
+    EditText quote;
+    EditText 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_user);
+
+
+
     }
 }
