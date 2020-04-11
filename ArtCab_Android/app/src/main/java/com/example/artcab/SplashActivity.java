d5dcb5e4844fd372e5d0cd510a07a08916a17049
Luke-Vader <32070588+Luke-Vader@users.noreply.github.com>
2020-04-11 18:26:09 +0530

    job fragment layout fixed

--- ArtCab_Android/app/src/main/java/com/example/artcab/SplashActivity.java
+++ ArtCab_Android/app/src/main/java/com/example/artcab/SplashActivity.java
@@ -8,7 +8,7 @@ import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 1500;
+    private static int SPLASH_TIME_OUT = 800;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
