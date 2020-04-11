e506b0776200bcd453c080263127c7625c81cbde
Luke-Vader <32070588+Luke-Vader@users.noreply.github.com>
2020-04-11 17:55:05 +0530

    splash duratoion changed

--- ArtCab_Android/app/src/main/java/com/example/artcab/SplashActivity.java
+++ ArtCab_Android/app/src/main/java/com/example/artcab/SplashActivity.java
@@ -4,13 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(this, MainActivity.class));
-        finish();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
